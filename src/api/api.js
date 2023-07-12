1649fb904d3634d5b7b3a91bcd19af11111552f6
Ten-Do <107281193+Ten-Do@users.noreply.github.com>
2023-07-12 19:38:50 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
 export const API_URL = process.env.PUBLIC_URL;
+import { API_ENDPOINTS } from "./apiEndpoints.js"
 
 const $api = axios.create({
   withCredentials: true,
@@ -21,7 +21,7 @@ $api.interceptors.response.use((config) => {
         if (error.response?.status == 401 && error.config && !error.config._isRetry) {
             originalRequest._isRetry = true;
             try {
-                const response = await axios.get(`${API_URL}/refresh`, { withCredentials: true })
+                const response = await axios.get(`${API_URL}/${API_ENDPOINTS.REFRESH_TOKEN}`, { withCredentials: true })
                 localStorage.setItem('token', response.data.accessToken);
                 return $api.request(originalRequest);
             } catch (e) {
